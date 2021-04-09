6684e8f98d530c45c5536a2e326f16bde66f0780
Andrej Kenda <andrej.baca14@gmail.com>
2021-04-09 15:17:01 +0200

    changed kbd rate to 300ms, disabled dead characters with xmodmap and changed font to firacode

--- .config/sxhkd/scratchpad_startup.sh
+++ .config/sxhkd/scratchpad_startup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # If you need to run a command with 2 levels of nesting, escape the double quotes e.g. \"
-zsh -is eval   "alias vim='nvim -u $HOME/.config/nvim/alternative.vim';
-                alias cat='bat --theme Solarized\ \(light\)'
-                export MANPAGER=\"sh -c 'col -bx | bat --theme Solarized\ \(light\) -l man -p'\"
+zsh -is eval   "alias vi='nvim -u $HOME/.config/nvim/alternative.vim';
+                alias cat='bat --theme Solarized\ \(light\)';
+                export MANPAGER=\"sh -c 'col -bx | bat --theme Solarized\ \(light\) -l man -p'\";
                 export VISUAL='nvim -u $HOME/.config/nvim/alternative.vim';
                 export EDITOR='nvim -u $HOME/.config/nvim/alternative.vim';"
 
