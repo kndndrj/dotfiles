58d23eff53ea1dc17e7bbd201ebb2d343fecef15
Andrej Kenda <andrej.baca14@gmail.com>
2021-04-14 15:53:43 +0200

    term size smaller, font size 10, scratchpad different vim config

--- .config/sxhkd/scratchpad_startup.sh
+++ .config/sxhkd/scratchpad_startup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # If you need to run a command with 2 levels of nesting, escape the double quotes e.g. \"
-zsh -is eval   "alias vi='nvim -u $HOME/.config/nvim/alternative.vim';
+zsh -is eval   "alias vi='nvim -u $HOME/.config/nvim/init_alt.lua';
                 alias cat='bat --theme Solarized\ \(light\)';
                 export MANPAGER=\"sh -c 'col -bx | bat --theme Solarized\ \(light\) -l man -p'\";
-                export VISUAL='nvim -u $HOME/.config/nvim/alternative.vim';
-                export EDITOR='nvim -u $HOME/.config/nvim/alternative.vim';"
+                export VISUAL='nvim -u $HOME/.config/nvim/init_alt.lua';
+                export EDITOR='nvim -u $HOME/.config/nvim/init_alt.lua';"
 
