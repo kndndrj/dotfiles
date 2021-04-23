dc34028bf218031a4ae908712f07ec392803c228
Andrej Kenda <andrej.baca14@gmail.com>
2021-04-23 10:08:21 +0200

    added config with secondary display in autorandr, fixed a few things

--- .config/autorandr/fallback.sh
+++ .config/autorandr/fallback.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo "Xft.dpi: 96" | xrdb -merge
+echo "Xft.dpi: 150" | xrdb -merge
 
 # Reset the background
 $HOME/.fehbg &
 
 ### MONITORS ###
-bspc monitor -d  1 2 3 4 5 6 7 8 9 10
+bspc monitor -d 1 2 3 4 5 6 7 8 9 10
 
 # bspwm border settings
 bspc config border_width                3
-bspc config window_gap                  -3
+bspc config window_gap                  3
 bspc config top_padding                 40
 
 # List of programs to start
