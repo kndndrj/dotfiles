bb16aebd5823e31083af52f5d8a1866468c52500
Andrej Kenda <andrej.baca14@gmail.com>
2021-05-03 12:50:56 +0200

    changed scratchpad colorscheme, added npmrc, removed fehbg command

--- .config/sxhkd/scratchpad_startup.sh
+++ .config/sxhkd/scratchpad_startup.sh
@@ -2,8 +2,8 @@
 
 # If you need to run a command with 2 levels of nesting, escape the double quotes e.g. \"
 zsh -is eval   "alias vi='nvim -u $HOME/.config/nvim/init_alt.lua';
-                alias cat='bat --theme Solarized\ \(light\)';
-                export MANPAGER=\"sh -c 'col -bx | bat --theme Solarized\ \(light\) -l man -p'\";
+                alias cat='bat --theme OneHalfLight';
+                export MANPAGER=\"sh -c 'col -bx | bat --theme OneHalfLight -l man -p'\";
                 export VISUAL='nvim -u $HOME/.config/nvim/init_alt.lua';
                 export EDITOR='nvim -u $HOME/.config/nvim/init_alt.lua';"
 
