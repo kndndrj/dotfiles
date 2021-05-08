61ffb4f65675b191fa46e218e4c31bcced3ed67d
Andrej Kenda <andrej.baca14@gmail.com>
2021-05-08 20:44:29 +0200

    background reset moved to common postswitch, caps:escape fix

--- .config/autorandr/postswitch.d/restart_programs.sh
+++ .config/autorandr/postswitch.d/restart_programs.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Reset the background
+feh --no-fehbg --randomize --bg-fill $(xdg-user-dir PICTURES)/Ozadja/* &
+
 # List of programs to start
 # Add multiple instances of the same program next to eachother
 # and put "heavy" programs at the end
