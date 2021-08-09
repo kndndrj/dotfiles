58c01e9b460ca45ebca1ef24d5e3c9fa33157bd8
Andrej Kenda <andrej.baca14@gmail.com>
2021-08-09 15:04:05 +0200

    added underline to window titles

--- .config/polybar/scripts/bspwm_window_titles.sh
+++ .config/polybar/scripts/bspwm_window_titles.sh
@@ -22,13 +22,17 @@ Pulsemixer
 # Presets
 display_name="class" # valid: name/class
 name_cut=15
+empty_message="..."
+underline_enabled="true"
 # Use colors as in polybar config (e.g. "#efefef"),
 # must be "-" if there is no color specified.
 foreground_focused="-"
-background_focused="#444"
+background_focused="-"
 foreground_unfocused="#999"
 background_unfocused="-"
+underline_color="#8bbaed"
 
+[ "$underline_enabled" = "true" ] && underline="%{u$underline_color}%{+u}"
 # Subscribe to events on which the window title will get updated
 bspc subscribe node_focus node_remove node_stack desktop_focus | while read -r _; do
     # Get active monitor, all windows and the focused window
@@ -54,7 +58,7 @@ bspc subscribe node_focus node_remove node_stack desktop_focus | while read -r _
 
             # Color the active window differently and put it at the begining
             if [ "$window_id" = "$window_focused_id" ]; then
-                curr_wins="%{F$foreground_focused B$background_focused} %{A3:bspc node $window_id --close:}${window_icon#* } ${window} %{A}%{F$foreground_unfocused B$background_unfocused}${curr_wins}"
+                curr_wins="%{F$foreground_focused B$background_focused}$underline %{A3:bspc node $window_id --close:}${window_icon#* } ${window} %{A}%{-u}%{F$foreground_unfocused B$background_unfocused}${curr_wins}"
             else
                 curr_wins="${curr_wins} %{A1:bspc node $window_id --focus:}%{A3:bspc node $window_id --close:}${window_icon#* } ${window} %{A}%{A}"
             fi
@@ -62,7 +66,7 @@ bspc subscribe node_focus node_remove node_stack desktop_focus | while read -r _
     done
 
     # Print to temp file and trigger the polybar hook
-    [ -z "$curr_wins" ] && curr_wins="..."
+    [ -z "$curr_wins" ] && curr_wins=$empty_message
     echo "$curr_wins" > "/tmp/bspwm_windows.${monitor}"
     polybar-msg hook windowtitles-bspwm 1
 
