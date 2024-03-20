7af28d88f040ca132fbbf646f710e19600e98f13
Andrej Kenda <andrej.baca14@gmail.com>
2024-03-20 17:54:58 +0100

    update linux config

--- linux/.config/sway/wrapper.sh
+++ linux/.config/sway/wrapper.sh
@@ -25,18 +25,6 @@ export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
 export PYLINTHOME="$XDG_CACHE_HOME/pylint"
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 
-# System color settings
-export COL_SYSTEM_PRIMARY="#D5AD18"
-export COL_SYSTEM_SECONDARY="#DCC080"
-export COL_SYSTEM_ALERT="#FF4545"
-export COL_SYSTEM_WARN="#FF875A"
-export COL_SYSTEM_FOREGROUND="#E9E8E8"
-export COL_SYSTEM_BACKGROUND="#444444"
-export COL_SYSTEM_FOREGROUND_ALT="#AAAAAA"
-export COL_SYSTEM_BACKGROUND_ALT="#1F1E1C"
-export COL_SYSTEM_BACKGROUND_TRANSPARENT="#bb${COL_SYSTEM_BACKGROUND#\#}"
-
-# Misc
 # Go
 export PATH="$PATH:$GOPATH/bin"
 # Java AWT apps
@@ -44,10 +32,6 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 # EFL apps
 export ECORE_EVAS_ENGINE=wayland_egl
 export ELM_ENGINE=wayland_egl
-# Replace env variables in configs
-envsubst < ~/.config/dunst/dunstrc_template > ~/.config/dunst/dunstrc
-envsubst < ~/.config/waybar/style_template.css > ~/.config/waybar/style.css
-envsubst '$COL_SYSTEM_PRIMARY $COL_SYSTEM_SECONDARY $COL_SYSTEM_ALERT $COL_SYSTEM_FOREGROUND $COL_SYSTEM_BACKGROUND $COL_SYSTEM_FOREGROUND_ALT $COL_SYSTEM_BACKGROUND_ALT $COL_SYSTEM_BACKGROUND_TRANSPARENT' < ~/.config/sway/config_template > ~/.config/sway/config
 
 # Waybar tray misc
 export XDG_CURRENT_DESKTOP=sway
